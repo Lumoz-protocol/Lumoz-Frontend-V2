1ef912b0a76d1b1d9b6a9a78f8cbdb00a9998f58
Xingrui Ni <315631006@qq.com>
2024-04-09 16:38:13 +0800

    F

--- components/common/scroll.vue
+++ components/common/scroll.vue
@@ -1,12 +1,10 @@
 <template>
   <client-only class="overflow-hidden">
-    <Vue3SeamlessScroll :list="list" class="scroll" direction="left" hover step="2">
-      <div class="flex">
-        <div v-for="(item, index) in list" :key="index">
-          <img :src="item" alt="" class="w-40 opacity-80 hover:(opacity-100) hvr-float mx-14 mt-2 cursor-pointer">
-        </div>
+    <Vue3Marquee  pauseOnHover class="flex items-center">
+      <div v-for="(item, index) in list" :key="index">
+        <img :src="item.img" alt="" class="opacity-80 hover:(opacity-100) hvr-float mt-2 cursor-pointer" :class="item.css">
       </div>
-    </Vue3SeamlessScroll>
+    </Vue3Marquee>
   </client-only>
 </template>
  
@@ -25,8 +23,65 @@ import M11 from '@/assets/img/backer/11.avif'
 import M12 from '@/assets/img/backer/12.avif'
 import M13 from '@/assets/img/backer/13.avif'
 import M14 from '@/assets/img/backer/14.avif'
-import { Vue3SeamlessScroll } from "vue3-seamless-scroll"
-const list = ref([M1, M2, M3, M4, M5, M6, M7, M8, M9, M10, M11, M12, M13, M14, M1, M2, M3, M4, M5, M6, M7, M8, M9, M10, M11, M12, M13, M14, M1, M2, M3, M4, M5, M6, M7, M8, M9, M10, M11, M12, M13, M14, M1, M2, M3, M4, M5, M6, M7, M8, M9, M10, M11, M12, M13, M14, M1, M2, M3, M4, M5, M6, M7, M8, M9, M10, M11, M12, M13, M14])
+import { Vue3Marquee } from 'vue3-marquee'
+const list = ref([
+  {
+    img: M1,
+    css: 'w-40 mx-10'
+  },
+  {
+    img: M2,
+    css: 'w-30 mx-10'
+  },
+  {
+    img: M3,
+    css: 'w-45 mx-10'
+  },
+  {
+    img: M4,
+    css: 'w-45 mx-10'
+  },
+  {
+    img: M5,
+    css: 'w-45 mx-10'
+  },
+  {
+    img: M6,
+    css: 'w-45 mx-10'
+  },
+  {
+    img: M7,
+    css: 'w-40 mx-10'
+  },
+  {
+    img: M8,
+    css: 'w-40 mx-10'
+  },
+  {
+    img: M9,
+    css: 'w-35 mx-10'
+  },
+  {
+    img: M10,
+    css: 'w-35 mx-10'
+  },
+  {
+    img: M11,
+    css: 'w-45 mx-10'
+  },
+  {
+    img: M12,
+    css: 'w-35 mx-10'
+  },
+  {
+    img: M13,
+    css: 'w-50'
+  },
+  {
+    img: M14,
+    css: 'w-35 mx-10'
+  }
+])
 </script>
  
 <style>
