c870864899798d090e1ca1dba5f26c91e74b86ec
Xingrui Ni <315631006@qq.com>
2024-04-30 10:35:09 +0800

    F

--- constants/networks.ts
+++ constants/networks.ts
@@ -26,7 +26,7 @@ export const NODE_NETWORKS = [
     symbol: 'USDC',
     decimals: 18,
     zkfAddress: '0xFed0Ea861B09a471cc6646c0E0b36863Be4d94e4',
-    depositContract: '0x3a59464d1c529b6D53086110Baaf99e605047b13',
+    depositContract: '0x5fF379cD12002c37eB1422C7a988A1CB7f6a17a2',
     rpcUrl: 'https://testnet-rpc.zkfair.io',
     explorer: 'https://testnet-scan.zkfair.io',
     img: ZKF
@@ -36,7 +36,7 @@ export const NODE_NETWORKS = [
     chainId: 686868,
     symbol: 'BTC',
     decimals: 18,
-    depositContract: '0xeed4dA035d084b0A0fdA04444270D9383E142452',
+    depositContract: '0xb54a0e66AE448A90b76cAc87d0Ee49a65F6CBb1e',
     rpcUrl: 'https://testnet-rpc.merlinchain.io',
     explorer: 'https://testnet-scan.merlinchain.io',
     img: MERL
@@ -46,7 +46,7 @@ export const NODE_NETWORKS = [
     chainId: 421614,
     symbol: 'ETH',
     decimals: 18,
-    depositContract: '0xeed4da035d084b0a0fda04444270d9383e142452',
+    depositContract: '0x5C9C8DEa9cBf5f1AdD4Cff89D07D15A62F37c711',
     rpcUrl: 'https://sepolia-rollup.arbitrum.io/rpc',
     explorer: 'https://sepolia.arbiscan.io',
     img: ARB
