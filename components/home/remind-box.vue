d3be800d495ecc03226c6aa6224ea9d8aeb6b5ea
Xingrui Ni <315631006@qq.com>
2024-06-15 21:25:25 +0800

    F

---
+++ components/home/remind-box.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="home-remind-box bg-filter-1 py-4 px-20 mx-2">
+    <div class="text-center text-[#FF6B00] text-xl font-bold">Nodes Buyback Guarantee: </div>
+    <div class="text-center mt-2">Lumoz offers a unique 80% Buyback Guarantee mechanism. </div>
+    <div class="flex items-center justify-center mt-2">
+        For details, please <span class="text-3xl mx-2">👉</span>see the <a class="ml-2 underline text-primary-900 opacity-90 hover:opacity-100 cursor-pointer" href="https://docs.lumoz.org/purchase-zkverifier-node-and-set-up/purchase-license/buyback-guarantee" target="_blank">rules.</a>
+    </div>
+</div>
+</template>
+<style scoped>
+.home-remind-box {
+    background:
+      url('@/assets/img/share/phase-left.avif') no-repeat, url('@/assets/img/share/phase-right.avif') no-repeat;
+    background-position: left 0 top 0, right 0 top 0;
+    background-size: auto 100%;
+    mix-blend-mode: lighten;
+}
+</style>
