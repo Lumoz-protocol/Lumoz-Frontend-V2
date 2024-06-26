0dc85cbaa329611f0ebaa2f1ce2d88e490fbd1b0
Xingrui Ni <315631006@qq.com>
2024-06-26 17:40:21 +0800

    F

--- components/home/remind-box.vue
+++ components/home/remind-box.vue
@@ -1,9 +1,12 @@
 <template>
-<div class="home-remind-box bg-filter py-4 px-20 mx-2">
+<div class="home-remind-box bg-filter py-4 md:px-20 mx-2 rounded-4xl md:w-150 overflow-hidden">
     <div class="text-center text-[#FF6B00] text-xl font-bold">Nodes Buyback Guarantee: </div>
-    <div class="text-center mt-2">Lumoz offers a unique 80% Buyback Guarantee mechanism. </div>
-    <div class="flex items-center justify-center mt-2">
-        For details, please <span class="text-3xl mx-2">👉</span>see the <a class="ml-2 underline text-primary-900 opacity-90 hover:opacity-100 cursor-pointer" href="https://docs.lumoz.org/purchase-zkverifier-node-and-set-up/purchase-license/buyback-guarantee" target="_blank">rules.</a>
+    <div class="text-center mt-2">
+        Lumoz offers a unique 80% Buyback Guarantee mechanism. The refund policy applies to retail users only.
+    </div>
+    <div class="flex items-center justify-center">
+        For details, please
+        <span class="text-2xl">👉 </span>see the <a class="ml-2 underline text-primary-900 opacity-90 hover:opacity-100 cursor-pointer" href="https://docs.lumoz.org/purchase-zkverifier-node-and-set-up/purchase-license/buyback-guarantee" target="_blank">rules.</a>
     </div>
 </div>
 </template>
