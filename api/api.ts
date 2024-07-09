6fa8c04db5bb974c8169d2768e5059fcce0018d3
Xingrui Ni <315631006@qq.com>
2024-07-09 22:31:33 +0800

    F

--- api/api.ts
+++ api/api.ts
@@ -1,18 +1,8 @@
-import request from './request.utils'
 import { DATA_API } from '@/constants/networks'
 import axios from 'axios'
-import { ElNotification } from 'element-plus'
 
 const getDataUrl = (url: string) => {
   return `${DATA_API}${url}`
-}
-
-export async function getAvaliableRollupList() {
-  try {
-    return (await request.get(getDataUrl(`rollup/available/list`))).result
-  } catch {
-    return []
-  }
 }
 
 export const getAchieveInfo = async (address: string, timestamp: string, signature: string) => {
